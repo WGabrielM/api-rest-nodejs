93b056a773b6601065113e03fee59cc13f7506df
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-06-18 15:02:33 -0300

    rollback project

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,3 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
 
-const app = fastify()
-
-app.get('/hello', () => {
-  return 'Hello World'
-})
-
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP Server Running!')
-  })
+const app = fastify();
