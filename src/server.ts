8a81dd876bd6e9f195af6d1cf0089089fb077ba0
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-07-19 08:47:52 -0300

    add variable enviroment

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,7 @@ import { knex } from './database';
 const app = fastify();
 
 app.get('/hello', async () => {
-    const transaction = await knex('transactions').insert({
-        id: crypto.randomUUID(),
-        title: 'Test Transaction',
-        amount: 1000,
-    }).returning('*')
+    const transaction = await knex('transactions').where('amount', 1000).select('*')
 
     return transaction
 })
