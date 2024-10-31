271bdeaa15d07ab2ba5f67cdefcea6ed74bb4a19
Wellington <wellingtongmb@gmail.com>
2024-10-31 10:13:06 -0300

    create route transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,25 +1,22 @@
 import { z } from 'zod';
-import { FastifyInstance } from "fastify"
-
 import { knex } from "../database"
+import { FastifyInstance } from "fastify"
 import { randomUUID } from 'node:crypto'
+
 
 export async function transactionsRoutes(app: FastifyInstance) {
     app.post('/', async (request, replay) => {
-        // { title, amount, type: credit or debit }
-
         const createTransactionsBodySchema = z.object({
             title: z.string(),
             amount: z.number(),
             type: z.enum(['credit', 'debit']),
         })
-
         const { title, amount, type } = createTransactionsBodySchema.parse(request.body)
 
         await knex('transactions').insert({
-                id: randomUUID(),
-                title,
-                amount: type === 'credit' ? amount : amount * -1,
+            id: randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1,
         })
 
         return replay.status(201).send()
