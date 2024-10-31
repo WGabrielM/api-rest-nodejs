b92a3028095e8b3a027cc5069e2751bcc4710588
Wellington <wellingtongmb@gmail.com>
2024-10-31 13:41:39 -0300

    create a preHandler for all methods in the route file

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -8,6 +8,10 @@ import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
+    app.addHook('preHandler', async (request, reply) => {
+        console.log(`[${request.method}] - ${request.url}`);
+    })
+
     app.get('/', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
         const { sessionId } = request.cookies
         const transactions = await knex('transactions').where('session_id', sessionId).select()
